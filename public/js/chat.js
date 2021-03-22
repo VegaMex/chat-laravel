d158be040a5e6a55b05f9a82b0a7cd2e8fc9db2e
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2021-03-22 13:15:38 -0600

    Sintaxis

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,95 +12,97 @@ let typingTimer = false;
 window.onload = function () {
  
   axios.get('/auth/user')
-    .then( res => {
+    .then(res => {
   
       authUser = res.data.authUser;
   
     })
     .then(() => {
- 
-      axios.get(`/chat/${chatId}/get_users`).then( res => {
+      axios.get(`/chat/${chatId}/get_users`).then(res => {
   
-        let results = res.data.users.filter( user => user.id != authUser.id);
+        let results = res.data.users.filter(user => user.id != authUser.id);
   
         if(results.length > 0)
+        {
           chatWith.innerHTML = results[0].name;
-  
+        }
+
       });
     })
     .then(() => {
-
       axios.get(`/chat/${chatId}/get_messages`).then(res => {
- 
         appendMessages(res.data.messages);
-   
       });
-
     })
     .then(() => {
-
-      Echo.join(`chat.${chatId}`)
+    Echo.join(`chat.${chatId}`)
       .listen('MessageSent', (e) => {
- 
-        appendMessage(
-          e.message.user.name,
-          PERSON_IMG,
-          'left',
-          e.message.content,
-          formatDate(new Date(e.message.created_at))
-        );
- 
-      }).here((users) => {
- 
-        let result = users.filter(user => user.id != authUser.id);
-     
-        if(result.length > 0)
-        {
-          chatStatus.className = 'chatStatus online';
-        }
-     
-      }).joining((user) => {
-        
-        if(user.id != authUser.id)
-        {
-          chatStatus.className = 'chatStatus online';
-        }
-     
-      }).leaving((user) => {
-        
-        if(user.id != authUser.id)
-        {
-          chatStatus.className = 'chatStatus offline';
-        }
-     
-      }).listenForWhisper('typing', (e) => {
-  
-        if(e > 0)
+
+      appendMessage(
+        e.message.user.name,
+        PERSON_IMG,
+        'left',
+        e.message.content,
+        formatDate(new Date(e.message.created_at))
+      );
+
+    }).here((users) => {
+
+      let result = users.filter(user => user.id != authUser.id);
+    
+      if(result.length > 0)
+      {
+        chatStatus.className = 'chatStatus online';
+      }
+    
+    }).joining((user) => {
+      
+      if(user.id != authUser.id)
+      {
+        chatStatus.className = 'chatStatus online';
+      }
+    
+    }).leaving((user) => {
+      
+      if(user.id != authUser.id)
+      {
+        chatStatus.className = 'chatStatus offline';
+      }
+    
+    }).listenForWhisper('typing', (e) => {
+
+      if(e > 0)
+      {
         typing.style.display = '';
- 
-        if(typingTimer) {
-            clearTimeout(typingTimer);
-        }
- 
-        typingTimer = setTimeout( () => {
-            
-            typing.style.display = 'none';
-    
-            typingTimer = false;
-    
-        }, 3000);
-    
-      });
+      }
+
+      if(typingTimer)
+      {
+        clearTimeout(typingTimer);
+      }
+
+      typingTimer = setTimeout(() => {
+          
+          typing.style.display = 'none';
+  
+          typingTimer = false;
+  
+      }, 3000);
+  
     });
+  });
 }
 
-msgerForm.addEventListener("submit", event => {
+msgerForm.addEventListener('submit', event => {
  
     event.preventDefault();
  
     const msgText = msgerInput.value;
  
-    if (!msgText) return;
+    if (!msgText)
+    {
+      return;
+    }
  
     axios.post('/message/sent', {
         message: msgText,
@@ -121,8 +123,8 @@ msgerForm.addEventListener("submit", event => {
  
 });
  
-function appendMessage(name, img, side, text, date) {
-
+function appendMessage(name, img, side, text, date)
+{
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
@@ -139,58 +141,49 @@ function appendMessage(name, img, side, text, date) {
   `;
  
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-
   scrollToBottom();
 }
 
-function appendMessages(messages) {
- 
+function appendMessages(messages) 
+{
   let side = 'left';
   
   messages.forEach(message => {
-  
-      side = (message.user_id == authUser.id) ? 'right' : 'left';
-  
-      appendMessage(
-          message.user.name, 
-          PERSON_IMG, 
-          side, 
-          message.content, 
-          formatDate(new Date(message.created_at)))
-  
+    side = (message.user_id == authUser.id) ? 'right' : 'left';
+    appendMessage(
+      message.user.name, 
+      PERSON_IMG, 
+      side, 
+      message.content, 
+      formatDate(new Date(message.created_at)))
   });
 
 }
- 
-Echo.join(`chat.${chatId}`).listen('MessageSent', (e) => {
-    
-  console.log(e);
 
-});
-
-function get(selector, root = document) {
+function get(selector, root = document) 
+{
   return root.querySelector(selector);
 }
  
-function formatDate(date) {
-    const d = date.getDate();
-    const mo = date.getMonth() + 1;
-    const y = date.getFullYear();
-    const h = "0" + date.getHours();
-    const m = "0" + date.getMinutes();
-    return `${d}/${mo}/${y} ${h.slice(-2)}:${m.slice(-2)}`;
+function formatDate(date) 
+{
+  const d = date.getDate();
+  const mo = date.getMonth() + 1;
+  const y = date.getFullYear();
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+  return `${d}/${mo}/${y} ${h.slice(-2)}:${m.slice(-2)}`;
 }
 
 function scrollToBottom()
 {
-    msgerChat.scrollTop = msgerChat.scrollHeight;
+  msgerChat.scrollTop = msgerChat.scrollHeight;
 }
 
-function sendTypingEvent() {
-
+function sendTypingEvent() 
+{
   typingTimer = true;
 
   Echo.join(`chat.${chatId}`)
     .whisper('typing', msgerInput.value.length);
-
 }
